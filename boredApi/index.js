c8df95d00601f51bc2ef15daf0e575b6c2fbde36
emirisato1003 <e.sato@students.clark.edu>
2025-03-02 12:06:48 -0800

    add eventlisner

--- boredApi/index.js
+++ boredApi/index.js
@@ -1,7 +1,12 @@
+const activityName = document.getElementById('activity-name');
+const btn = document.getElementById('btn');
 
-const url = 'https://apis.scrimba.com/bored/api/activity'
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        document.getElementById('activity-name').textContent = data.activity
-    })
+btn.addEventListener('click', () => {
+    const url = 'https://apis.scrimba.com/bored/api/activity';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            activityName.textContent = data.activity;
+        });
+})
+
