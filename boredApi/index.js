6f89351b6d802f9b7566017afba10b88aacc233b
emirisato1003 <e.sato@students.clark.edu>
2025-03-02 12:09:55 -0800

    change style when button clicked

--- boredApi/index.js
+++ boredApi/index.js
@@ -2,6 +2,7 @@ const activityName = document.getElementById('activity-name');
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
+    document.getElementById('title').textContent = 'FunBot🎮'
     const url = 'https://apis.scrimba.com/bored/api/activity';
     fetch(url)
         .then(res => res.json())
