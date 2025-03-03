c24000d1c15df8cb5789cb8306251491327c3370
emirisato1003 <emirisato.1003@gmail.com>
2025-03-03 10:18:55 -0800

    add input form

--- blog/index.js
+++ blog/index.js
@@ -1,12 +1,17 @@
+
+
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts", { method: "GET" })
     .then((res) => res.json())
     .then((data) => {
         const postsArr = data.slice(0, 5);
         const posts = postsArr.map(post => {
             return (`<div class="singlePost">
-        <h1>${post.title}</h1>
-        <p>${post.body}</p>
-    </div>`);
+            <h1>${post.title}</h1>
+            <p>${post.body}</p>
+            </div>`);
         });
         document.getElementById('blog').innerHTML = posts.join('');
     });
+
+document.querySelector('button').addEventListener("click", () => console.log('clicked'));
